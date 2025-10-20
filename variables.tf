9a344d6806209b0677c8501d91ffaf7d4a1e5f38
jxilong <jxilong@cn.ibm.com>
2025-10-20 14:12:19 +0800

    fix variable

--- variables.tf
+++ variables.tf
@@ -6,9 +6,16 @@ variable "project_id" {
 variable "region" {
   description = "GCP 区域"
   type        = string
+}
 
 variable "worker_count" {
   description = "DataNode 数量"
   type        = number
   default     = 2
+}
+
+variable "allowed_source_ips" {
+  description = "允许访问 Web UI 的源 IP"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
 }
